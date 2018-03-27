94e8f95c77a0cea06c88cfda55b817b51a072d09
GiovanniAquila <giovanni.aquila@studenti.polito.it>
2018-03-27 10:39:58 +0200

    prova modifica

--- ShareFileLAN/Form1.Designer.cs
+++ ShareFileLAN/Form1.Designer.cs
@@ -28,41 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-
             this.button1 = new System.Windows.Forms.Button();
             this.checkBoxVisibilità = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(820, 635);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(333, 719);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(466, 117);
+            this.button1.Size = new System.Drawing.Size(621, 146);
             this.button1.TabIndex = 0;
             this.button1.Text = "START";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-
+            // 
+            // checkBoxVisibilità
+            // 
             this.checkBoxVisibilità.AutoSize = true;
             this.checkBoxVisibilità.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBoxVisibilità.Location = new System.Drawing.Point(1064, 66);
+            this.checkBoxVisibilità.Location = new System.Drawing.Point(1419, 82);
+            this.checkBoxVisibilità.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBoxVisibilità.Name = "checkBoxVisibilità";
             this.checkBoxVisibilità.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.checkBoxVisibilità.Size = new System.Drawing.Size(222, 29);
+            this.checkBoxVisibilità.Size = new System.Drawing.Size(306, 35);
             this.checkBoxVisibilità.TabIndex = 1;
             this.checkBoxVisibilità.Text = "Visibile agli altri utenti";
             this.checkBoxVisibilità.TextAlign = System.Drawing.ContentAlignment.TopLeft;
             this.checkBoxVisibilità.UseVisualStyleBackColor = true;
             this.checkBoxVisibilità.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-
-            this.ClientSize = new System.Drawing.Size(1328, 799);
+            this.ClientSize = new System.Drawing.Size(1771, 999);
             this.Controls.Add(this.checkBoxVisibilità);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "ShareFileLAN";
             this.Load += new System.EventHandler(this.Form1_Load);
