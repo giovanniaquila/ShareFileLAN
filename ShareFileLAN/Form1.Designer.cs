4ba62413318482fcbc7e81bdce0d6d39b97e1559
GiovanniAquila <giovanni.aquila@studenti.polito.it>
2018-03-27 10:46:32 +0200

    Prova2

--- ShareFileLAN/Form1.Designer.cs
+++ ShareFileLAN/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(333, 719);
+            this.button1.Location = new System.Drawing.Point(1089, 750);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(621, 146);
